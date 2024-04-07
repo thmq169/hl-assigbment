7a92ad0dd7d8c1d74cfe8d47b42dc0e5f2508f6b
thmq169 <88388218+thmq169@users.noreply.github.com>
2024-04-07 17:47:58 +0700

    Update path images

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="flex justify-between">
                 <div>
                     <Link to="/">
-                        <img className="h-[5rem]" src="/src/assets/logo.png" />
+                        <img className="h-[5rem]" src="/logo.png" />
                     </Link>
                 </div>
                 <div className="flex items-center space-x-3 mr-4 md:mr-0">
@@ -15,7 +15,7 @@ const Header = () => {
                         <h4 className="text-base">Jim HLS</h4>
                     </div>
                     <div className="overflow-hidden rounded-full w-14 h-14 flex items-center justify-startx">
-                        <img src="/src/assets/avatar.jpg" alt="" className="object-cover block" />
+                        <img src="/avatar.jpg" alt="" className="object-cover block" />
                     </div>
                 </div>
             </div>
